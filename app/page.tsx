46b6ec581413ce46bfd2b4278af01d29198db8b3
quangtp <quangtran.180495@gmail.com>
2025-09-17 22:28:19 +0700

    test b

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <main>
       <h1>Next.js CI/CD Demo</h1>
-      <p>Xin chào! Đây là ứng dụng Next.js tối giản để test CI/CD.</p>
+      <p>Xin chào! Đây là ứng dụng Next.js tối giản để test CI/CD 1.</p>
       <ul>
         <li>Build và test bằng GitHub Actions</li>
         <li>Deploy tĩnh qua GitHub Pages (gh-pages)</li>
